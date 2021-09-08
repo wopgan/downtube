50aba7ccc64ecbecf0a0b6fb903c832e2291c273
Sylvio Lima <sylviolimas@gmail.com>
2021-09-07 23:24:47 -0300

    Add files via upload

--- downtube.py
+++ downtube.py
@@ -1,20 +1,36 @@
 from pytube import YouTube
 import os
 
-def downTube(videourl, path):
-    yt = YouTube(videourl)
-    confirm = input("Deseja baixar apenas o audio? s/n: ") 
-    if confirm == "n" or "N":
-        yt = yt.streams.filter(progressive=True, file_extension='mp4'). order_by('resolution').desc().first()
-    elif conffime == "s" or "S":
-        yt = yt.streams.filter(only_audio=True).first()
 
+def downTubeVideo(videourl, path):
+
+    yt = YouTube(videourl)
+    yt = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+    if not os.path.exists(path):
+        os.makedirs(path)
+    yt.download(path)
+    print(yt.title)
+
+
+def downTubeMusic(videourl, path):
+
+    yt = YouTube(videourl)
+    yt = yt.streams.filter(only_audio=True).first()
     if not os.path.exists(path):
         os.makedirs(path)
     yt.download(path)
 
-file = input("Digite o link para download do arquivo: ")
-user = input("Digite o usuário que você está usando: ")
-path = (f"/home/{user}/Downloads/DownTube")
 
-downTube(file, path)
+video = input("Digite o link do vídeo desejado: ")
+confirm = input("Deseja baixar só o audio? s/n: ")
+user = input("Digite o nome do seu usuário: ")
+
+
+if confirm == "s":
+    path = (f"/home/{user}/Música/DownTube")
+    downTubeMusic(video, path)
+    print(f"Salvo em {path}")
+elif confirm == "n":
+    path = (f"/home/{user}/Vídeos/DownTube")
+    downTubeVideo(video, path)
+    print(f"Salvo em {path}")
