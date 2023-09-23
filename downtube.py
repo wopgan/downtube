d8cfe8b90f254277f9b53e6a75fe89ea5dc7422c
TheManRafael <minecraft.hu.1234@gmail.com>
2023-09-23 18:22:38 +0200

    Improved code
    Optimized code, made it windows compatible and added errors.

--- downtube.py
+++ downtube.py
@@ -1,36 +1,40 @@
-from pytube import YouTube
 import os
+from pytube import YouTube
 
+def download_video(video_url, output_path):
+    try:
+        yt = YouTube(video_url)
+        stream = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+        if not os.path.exists(output_path):
+            os.makedirs(output_path)
+        stream.download(output_path)
+        print(f"Video downloaded successfully to: {output_path}/{yt.title}.mp4")
+    except Exception as e:
+        print("An error occurred while downloading the video:", str(e))
 
-def downTubeVideo(videourl, path):
+def download_music(video_url, output_path):
+    try:
+        yt = YouTube(video_url)
+        stream = yt.streams.filter(only_audio=True).first()
+        if not os.path.exists(output_path):
+            os.makedirs(output_path)
+        stream.download(output_path)
+        print(f"Audio downloaded successfully to: {output_path}/{yt.title}.mp4")
+    except Exception as e:
+        print("An error occurred while downloading the audio:", str(e))
 
-    yt = YouTube(videourl)
-    yt = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
-    if not os.path.exists(path):
-        os.makedirs(path)
-    yt.download(path)
-    print(yt.title)
+if __name__ == "__main__":
+    home = os.path.expanduser("~")
+    video_url = input("Enter the video URL: ").strip()
+    download_audio = input("Do you want to download audio only? (y/n): ").strip().lower()
 
-
-def downTubeMusic(videourl, path):
-
-    yt = YouTube(videourl)
-    yt = yt.streams.filter(only_audio=True).first()
-    if not os.path.exists(path):
-        os.makedirs(path)
-    yt.download(path)
-
-
-home = os.environ['HOME']
-video = input("Digite o link do vídeo desejado: ")
-confirm = input("Deseja baixar só o audio? s/n: ")
-
-
-if confirm == "s":
-    path = (f"{home}/Música/DownTube")
-    downTubeMusic(video, path)
-    print(f"Salvo em {path}")
-elif confirm == "n":
-    path = (f"{home}/Vídeos/DownTube")
-    downTubeVideo(video, path)
-    print(f"Salvo em {path}")
+    if download_audio == "y":
+        path = os.path.join(home, "Music", "DownTube")
+        download_music(video_url, path)
+        print(f"Saved in: {path}/{yt.title}.mp4")
+    elif download_audio == "n":
+        path = os.path.join(home, "Videos", "DownTube")
+        download_video(video_url, path)
+        print(f"Saved in: {path}/{yt.title}.mp4")
+    else:
+        print("Invalid choice. Please enter 'y' or 'n'.")
